54b4b817a38db6b2cf00ba54fe4a135a6fa85a2f
Shu <121787815+Ryothebassist@users.noreply.github.com>
2025-07-27 19:58:55 -0500

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -8,13 +8,3 @@ export async function storeDiscordTokens(userId, tokens) {
 export async function getDiscordTokens(userId) {
   return store.get(`discord-${userId}`);
 }
-
-// Store the list of users in the linked role
-export async function storeLinkedUsers(userIds) {
-  await store.set('linked-users', userIds);
-}
-
-// Get the list of users in the linked role
-export async function getLinkedUsers() {
-  return store.get('linked-users') || [];
-}
